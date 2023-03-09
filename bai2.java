90aafc2643cd5cccf4e07559b937f2209e872a4d
Nguyen Anh Tu <nguyentu822003@gmail.com>
2023-03-09 23:29:52 +0700

    Update bai2.java

--- bai2.java
+++ bai2.java
@@ -1,4 +1,17 @@
-
+import java.util.Scanner;
 public class bai2 {
-
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap so nguyen:");
+		int a = sc.nextInt();
+		if(a%2 == 0)
+		{
+			System.out.println("So vua nhap la so chan");
+		}
+		else
+		{
+			System.out.println("So vua nhap la so le");
+		}
+	}
 }
